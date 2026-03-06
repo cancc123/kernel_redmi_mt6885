da3f9d72e1ff762f088bed117a12787097a5ae5a
CCAN <2868353951@qq.com>
2026-03-06 20:44:27 +0800

    Update rtnetlink.c

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -15,6 +15,7 @@
  *	Fixes:
  *	Vitaly E. Lavrov		RTA_OK arithmetics was wrong.
  */
+#include <linux/rtnetlink.h>
 #include <linux/stacktrace.h>
 #include <linux/bitops.h>
 #include <linux/errno.h>
