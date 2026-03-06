b640cda8566ee431d7723d6dc0e06da0e4812fb1
CCAN <2868353951@qq.com>
2026-03-06 20:29:10 +0800

    Update dev.c

--- net/core/dev.c
+++ net/core/dev.c
@@ -71,7 +71,7 @@
  *              J Hadi Salim    :       - Backlog queue sampling
  *				        - netif_rx() feedback
  */
-
+#include <linux/stacktrace.h>
 #include <linux/uaccess.h>
 #include <linux/bitops.h>
 #include <linux/capability.h>
